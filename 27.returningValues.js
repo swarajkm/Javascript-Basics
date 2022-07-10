940394b5b0ffda0306bcf213d3a8ba0b842d7a4b
Swaraj Kumar Mohapatra <102534144+swarajkm@users.noreply.github.com>
2022-07-10 16:11:31 +0530

    assign return values

--- 27.returningValues.js
+++ 27.returningValues.js
@@ -3,3 +3,11 @@ function sum(a, b) {
 }
 
 console.log(sum(5, 6));
+
+var sum1 = 0;
+function addThree() {
+  sum1 = sum1 + 3;
+  //this function doesnt return anything hence if we log it the value will be undefined
+}
+
+console.log(addThree());
---
+++ 28. assignReturnValue.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+  return a + b;
+}
+
+var sumOfTwoNos = sum(10, 15);
+console.log(sumOfTwoNos);
